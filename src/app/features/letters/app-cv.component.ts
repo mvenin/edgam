e61a6af296d71b91ee1b5a964af82bf0257dda33
Marian VENIN <marian.venin@ext.ec.europa.eu>
2022-08-29 13:05:22 +0200

    add .fr support

--- src/app/features/letters/app-cv.component.ts
+++ src/app/features/letters/app-cv.component.ts
@@ -11,20 +11,55 @@ export class AppCvComponent {
 
   @ViewChild('txtDS') txtDS: ElementRef | undefined;
   sentenceLetters: any[] = [];
-  lettersMeta: any[] = [];
-
   boxToggle: boolean = false;
 
-  onToggleLetters($event: MouseEvent) {
-    this.sentenceLetters = [...this.txtDS?.nativeElement.value].map(t => t.toUpperCase());
+  isLetterA(ch: string): boolean {
+    let m = ch.toUpperCase().match(/^A|Â|À$/g);
+    return !!m;
+  }
 
-    let btn: any = $event.target;
-    this.boxToggle = !this.boxToggle;
-    btn['innerText'] = this.boxToggle ? 'Txt' : 'Go';
+  isLetterE(ch: string): boolean {
+    let m = ch.toUpperCase().match(/^E|Ë|É|È|Ê$/g);
+    return !!m;
+  }
+
+  isLetterI(ch: string): boolean {
+    let m = ch.toUpperCase().match(/^I|Î|Ï$/g);
+    return !!m;
+  }
+
+  isLetterO(ch: string): boolean {
+    let m = ch.toUpperCase().match(/^O|Ô$/g);
+    return !!m;
+  }
+
+  isLetterU(ch: string): boolean {
+    let m = ch.toUpperCase().match(/^Û|U|Ù|Ü$/g);
+    return !!m;
+  }
+
+  matchLetterA(val: string, char: string): boolean {
+    return this.isLetterA(char) && this.isLetterA(val);
+  }
+
+  matchLetterE(val: string, char: string): boolean {
+    return this.isLetterE(char) && this.isLetterE(val);
+  }
+
+  matchLetterI(val: string, char: string): boolean {
+    return this.isLetterI(char) && this.isLetterI(val);
+  }
+
+  matchLetterO(val: string, char: string): boolean {
+    return this.isLetterO(char) && this.isLetterO(val);
+  }
+
+  matchLetterU(val: string, char: string): boolean {
+    return this.isLetterU(char) && this.isLetterU(val);
   }
 
   isVowel(ch: string): boolean {
-    let found = ['A', 'E', 'I', 'O', 'U', 'Y'].find(t => t.toUpperCase() === ch.toUpperCase());
+    let found = ['A', 'Â', 'À', 'Ë', 'E', 'É', 'È', 'Ê', 'I', 'Î', 'Ï', 'Ô', 'O', 'Û', 'U', 'Ù', 'Ü', 'Y'].find(t => t.toUpperCase() === ch.toUpperCase());
     return !!found;
   }
 
@@ -39,13 +74,27 @@ export class AppCvComponent {
 
   isBreakToNewLine(ch: string) {
     let m = ch.match(/^#$/g);
-    console.debug(ch+' '+m);
     return m;
+  }
+
+  onToggleLetters($event: MouseEvent) {
+    this.sentenceLetters = [...this.txtDS?.nativeElement.value].map(t => t.toUpperCase());
+
+    let btn: any = $event.target;
+    this.boxToggle = !this.boxToggle;
+    btn['innerText'] = this.boxToggle ? 'Txt' : 'Go';
   }
 
   onTypeLetter($event: KeyboardEvent, textBox: HTMLInputElement, char: string) {
     let val = textBox.value;
-    if (val.toLowerCase() === char.toLowerCase() && this.isVowel(char)) {
+    if (this.isVowel(char) && (val.toLowerCase() === char.toLowerCase()
+      || this.matchLetterA(val, char)
+      || this.matchLetterE(val, char)
+      || this.matchLetterI(val, char)
+      || this.matchLetterO(val, char)
+      || this.matchLetterU(val, char)
+    )
+    ) {
       textBox.value = char.toUpperCase();
     } else {
       textBox.value = '';
