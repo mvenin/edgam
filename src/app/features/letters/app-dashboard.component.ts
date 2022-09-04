4b511f8d4b4cf07408813d489b07146189a500e3
Marian VENIN <marian.venin@ext.ec.europa.eu>
2022-09-04 11:42:08 +0200

    change dashboard css class

--- src/app/features/letters/app-dashboard.component.ts
+++ src/app/features/letters/app-dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component({
-  templateUrl: './app-dashboard.component.html'
+  templateUrl: './app-dashboard.component.html',
+  styleUrls: ['./app-dashboard.component.scss']
 })
 export class AppDashboardComponent {
 
